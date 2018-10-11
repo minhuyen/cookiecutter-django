a2989ff69c023dc7924fafa3eb57ba6444033223
Uyen Minh  Do <minhuyendo@gmail.com>
2018-10-11 09:16:08 +0700

    - update dockerfile django

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/utils.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/utils.py
@@ -3,6 +3,7 @@ from rest_framework.views import exception_handler
 
 logger = logging.getLogger(__name__)
 
+
 def custom_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
